199b6e230e49d598345e03a23e1e93fb37366cd4
Chuck <chuckgao0223@gmail.com>
2022-11-17 13:53:18 +0800

    done

--- src/index.js
+++ src/index.js
@@ -20,6 +20,6 @@ app.use(protectedRouter.allowedMethods())
 
 app.on('error', errorHandle)
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`服务器启动成功: http://localhost:${PORT}`)
 })
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,15 @@ const types = require('../app/constants')
 async function verifyAuth(ctx, next) {
   const { authorization } = ctx.request.header
 
-  const { uid, exp } = jwt.decode(authorization.slice(7))
+  let uid, exp
+
+  try {
+    ;({ uid, exp } = jwt.decode(authorization.slice(7)))
+  } catch (error) {
+    const err = new Error(types.EXPIRED)
+    ctx.app.emit('error', err, ctx)
+    return
+  }
 
   // 登录过期
   if (exp * 1000 < Date.now()) {
